5670235d139e4395b5dba725a4cec75843c66b1e
Nicolas Caramelli <caramelli.devel@gmail.com>
2023-01-30 11:03:11 +0100

    Merge dfb_types.h into directfb.h

--- src/directfb_result.c
+++ src/directfb_result.c
@@ -16,9 +16,9 @@
    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
 */
 
-#include <dfb_types.h>
 #include <direct/result.h>
 #include <direct/util.h>
+#include <directfb.h>
 
 /**********************************************************************************************************************/
 
